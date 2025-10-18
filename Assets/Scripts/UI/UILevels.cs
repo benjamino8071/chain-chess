8d8e2707d35ff74a4fdb8bee14a80178e5b04642
Ben <bpc23@sussex.ac.uk>
2025-10-18 14:41:37 +0100

    Section 3 levels DONE

--- Assets/Scripts/UI/UILevels.cs
+++ Assets/Scripts/UI/UILevels.cs
@@ -56,6 +56,8 @@ public class UILevels : UIPanel
         {
             _audioSystem.PlayUISignificantClickSfx();
             
+            _uiSystem.HideLeftBotSideUI();
+            
             _turnSystem.LoadLevelRuntime(_levelToLoad);
         });
         
